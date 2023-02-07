ed8e203b1a3fab1e8e4ce59ebccd1d9615b2d496
anton-karlovskiy <antonkarlovskiy@outlook.com>
2023-02-07 14:39:50 +0100

    chore: clean up

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,5 +1,5 @@
 // Address of the NFT Contract that you deployed
 const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS =
-  "0x6805a66179283Fa1FcEc8D89f4e162bebe5C0617";
+  "0xB6C572B5a288692F7a19E455d252c072C3bA9E38";
 
 module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
